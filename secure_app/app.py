2e943917420d0b62a8d24774bcc5df63943573b8
abhiii265 <pattiriabhi@gmail.com>
2026-04-03 00:15:12 +0530

    final

--- secure_app/app.py
+++ secure_app/app.py
@@ -4,23 +4,18 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # --- THE FACULTY DEMO SWITCH ---
-
-# 1. TO GET A "RED" ERROR (FAILED BUILD): 
-#    Remove the '#' so the line below is active.
-ADMIN_PASSWORD = "Sreenidhi_Test_Pass_123"
-
-# 2. TO GET A "GREEN" SUCCESS (PASSED BUILD): 
-#    Add a '#' to the start of the line above.
+# To trigger a RED FAIL: Keep the line below active.
+# To trigger a GREEN SUCCESS: Put a '#' in front of the line below.
+ADMIN_PASSWORD = "AKIA5S6D7F8G9H0J1K2L_SECRET_KEY_EXPOSED" 
 
 @app.route('/')
 def home():
-    # This 'globals().get' is the secret! 
-    # It prevents the "NameError" if the variable is commented out.
+    # Safe check using globals()
     if globals().get('ADMIN_PASSWORD'):
-        return render_template('index.html', vulnerable=True, error="CRITICAL: Hardcoded Credential Detected!")
+        return render_template('index.html', vulnerable=True, error="CRITICAL: Hardcoded AWS Credential Detected!")
     
     return render_template('index.html', vulnerable=False)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)  # nosec
-    # Triggering Pipeline for Faculty Demo.
+    # We removed # nosec here so Bandit flags the host binding too
+    app.run(host='0.0.0.0', port=8080)
